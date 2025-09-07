967d941757d4085d2f0950bac967a9767a1ad7b2
Vensen <sibandavensen@gmail.com>
2025-09-06 23:07:15 -0500

    profile fixed

--- build.sh
+++ build.sh
@@ -10,3 +10,10 @@ python manage.py collectstatic --no-input
 
 # Run database migrations
 python manage.py migrate
+
+# Add to build.sh after migrations
+python manage.py shell -c "
+from claims.models import UserProfile
+UserProfile.objects.all().update(profile_picture=None)
+print('Profile pictures reset')
+"
