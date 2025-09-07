45564a8c83dcee38e294ab8e2d840f70414d4cc1
Vensen <sibandavensen@gmail.com>
2025-09-06 19:21:16 -0500

    none

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
-#!/usr/bin/env bash
 # Exit on error
 set -o errexit
 
@@ -18,14 +10,3 @@ python manage.py collectstatic --no-input
 
 # Run database migrations
 python manage.py migrate
-
-# Create superuser automatically (only if none exists)
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='sSmith').exists():
-    User.objects.create_superuser('VensenSia', 'sibandavensen@gmail.com', 'Mzeki123456?')
-    print('Superuser created successfully')
-else:
-    print('Superuser already exists')
-"
